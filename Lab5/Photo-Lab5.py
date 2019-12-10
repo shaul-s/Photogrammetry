63a7daa3f9308e1a986040104e6e6a0e250333b1
Saul <Saul@DESKTOP>
2019-12-10 10:57:11 +0200

    almost finished 1st part

--- Lab5/Photo-Lab5.py
+++ Lab5/Photo-Lab5.py
@@ -1,7 +1,30 @@
-import reader as rd
+import numpy as np
+import Reader as rd
+import Camera as cam
+import SingleImage as sg
+import MatrixMethods as mm
+from scipy import linalg as la
 
-cam = rd.Reader.ReadCamFile(r'C:\Users\Saul\Desktop\lab5-shaul&ariel\PythonFiles\PythonFiles\rc30.cam')
-fiducialsImg = rd.Reader.Readtxtfile(r'C:\Users\Saul\Desktop\lab5-shaul&ariel\PythonFiles\PythonFiles\fiducialsImg.txt')
 
-print(cam,'\n')
-print(fiducialsImg)
+if __name__ == "__main__":
+
+    cam_pars = rd.Reader.ReadCamFile(r'rc30.cam')
+    fiducialsImg = rd.Reader.Readtxtfile(r'fiducialsImg.txt')
+
+    cam = cam.Camera(cam_pars["f"], np.array([[cam_pars["xp"]], [cam_pars["yp"]]]), None, None, cam_pars["fiducials"])
+    image = sg.SingleImage(cam)
+
+    inner_pars = image.ComputeInnerOrientation(fiducialsImg)
+    sig = np.dot(np.transpose(inner_pars["residuals"]), inner_pars["residuals"])/(len(inner_pars["residuals"])-6)
+
+
+
+
+
+    # printing answers
+    mm.PrintMatrix(inner_pars["params"], 'Inner Orientation Parameters')
+    mm.PrintMatrix(inner_pars["residuals"], 'Inner Orientation Parameters')
+    print(sig, '\n')
+
+    print(image.ComputeInverseInnerOrientation())
+    mm.PrintMatrix(sig * la.inv(inner_pars["N"]), 'var-cov', 8)
