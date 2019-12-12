7474de2a8f12f3229c422d6c37c153a4808b3158
Saul <shaul.s@campus.technion.ac.il>
2019-12-12 14:52:00 +0200

    almost finished

--- Lab5/Photo-Lab5.py
+++ Lab5/Photo-Lab5.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     cam_pars = rd.Reader.ReadCamFile(r'rc30.cam')
     fiducialsImg = rd.Reader.Readtxtfile(r'fiducialsImg.txt')
     control_p = rd.Reader.photoModXMLReader(r'lab5.xml')
+    control_p2 = rd.Reader.photoModXMLReader(r'Lab5P7-9.xml')
 
     cam = cam.Camera(cam_pars["f"], np.array([[cam_pars["xp"]], [cam_pars["yp"]]]), None, None, cam_pars["fiducials"])
     image = sg.SingleImage(cam)
@@ -22,7 +23,9 @@ if __name__ == "__main__":
 
     ###   PART B - EXTERIOR ORIENTATION  ###
     fiducialsImg3574 = np.zeros((2, int(len(control_p[2])/2)))  # initializing the fiducials array for img 3574
+    fiducialsImg3574_xtra = np.zeros((2, int(len(control_p2[2]))))
     GCP = np.zeros((3, int(len(control_p[1]))))
+    GCP_xtra = np.zeros((3, int(len(control_p2[1]))))
     j = 0
     for row in control_p[2]:
         if row[0] == '3574':
@@ -35,14 +38,25 @@ if __name__ == "__main__":
         GCP[1, i] = row[3]
         GCP[2, i] = row[4]
 
-    fiducialsImg3574 = fiducialsImg3574.T
+    for i, row in enumerate(control_p2[2]):
+        fiducialsImg3574_xtra[0, i] = row[1]
+        fiducialsImg3574_xtra[1, i] = row[2]
 
+
+    for i, row in enumerate(control_p2[1]):
+        GCP_xtra[0, i] = row[2]
+        GCP_xtra[1, i] = row[3]
+        GCP_xtra[2, i] = row[4]
+
+    fiducialsImg3574 = fiducialsImg3574.T
+    fiducialsImg3574_xtra = fiducialsImg3574_xtra.T
     fiducialsImg3574B = np.delete(fiducialsImg3574, 4, 0)
     GCPB = np.delete(GCP, 4, 1)
 
 
     extOrientationA = image.ComputeExteriorOrientation(fiducialsImg3574, GCP, 1e-6)
     extOrientationB = image.ComputeExteriorOrientation(fiducialsImg3574B, GCPB, 1e-6)
+    extOrientationC = image.ComputeExteriorOrientation(np.vstack((fiducialsImg3574[0:3,:], fiducialsImg3574_xtra)), np.hstack((GCP[:,0:3], GCP_xtra)), 1e-6)
     extOrientationD = image.ComputeExteriorOrientation(fiducialsImg3574[0:3,:], GCP[:,0:3], 1e-6)
 
 
