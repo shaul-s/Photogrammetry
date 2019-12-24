342343afe5a9a5029ee7488083a6a268f8b732e1
Saul <Saul@DESKTOP>
2019-12-24 11:36:36 +0200

    lab 7 progress

--- Lab7/Photo-Lab7.py
+++ Lab7/Photo-Lab7.py
@@ -20,3 +20,18 @@ if __name__ == "__main__" :
 
     # creating two camera objects
     cam1 = cam.Camera(focal * pixel_size, np.array([xp * pixel_size, yp * pixel_size]), None, None, None)
+    image2008 = sg.SingleImage(cam1)
+    img2008points1 = rd.Reader.ReadSampleFile(r'IMG_2008_1.json')
+    img2009points1 = rd.Reader.ReadSampleFile(r'IMG_2009_1.json')
+    img2008points1_tmp = img2008points1
+    img2009points1_tmp = img2009points1
+    #  adjusting to camera system in pixels
+    for i in range(len(img2008points1)) :
+        img2008points1_tmp[i, 1] = 3648 - img2008points1_tmp[i, 1]
+        img2009points1_tmp[i, 1] = 3648 - img2009points1_tmp[i, 1]
+    #  turn pixels to mm's
+    img2008points1mm = img2008points1_tmp * pixel_size
+    img2009points1mm = img2009points1_tmp * pixel_size
+    #  now we have our fiducials in mm's !
+
+    print('hi')
