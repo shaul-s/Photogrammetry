ca1db4005ca3e95db1a30d891242fdf48a966024
Saul <Saul@DESKTOP>
2020-01-05 11:35:20 +0200

    saving progress lab 8

---
+++ Lab8/Photo-Lab8.py
@@ -0,0 +1,10 @@
+import numpy as np
+import Reader as rd
+import Camera as cam
+import SingleImage as sg
+import ImagePair as ip
+from matplotlib import pyplot as plt
+import MatrixMethods as mm
+from scipy import linalg as la
+
+if __name__ == "__main__" :
