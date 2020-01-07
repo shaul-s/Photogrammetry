18bc101808d358be6a8ed1a7a29926a3c67e593f
Saul <shaul.s@campus.technion.ac.il>
2020-01-07 12:14:20 +0200

    lab 8 update

---
+++ ImageTriple.py
@@ -0,0 +1,98 @@
+import numpy as np
+from ImagePair import ImagePair
+from SingleImage import SingleImage
+from Camera import Camera
+
+
+class ImageTriple(object):
+    def __init__(self, imagePair1, imagePair2):
+        """
+        Inisialize the ImageTriple class
+
+        :param imagePair1: first image pair
+        :param imagePair2: second image pair
+
+        .. warning::
+
+            Check if the relative orientation is solved for each image pair
+        """
+        self.__imagePair1 = imagePair1
+        self.__imagePair2 = imagePair2
+        self.__isSolved = False  # Flag for the relative orientation for both models
+
+        @property
+        def isSolved(self):
+            """
+            Flag for the relative orientation of both models
+            returns True if the relative orientation is solved, otherwise it returns False
+
+            :return: boolean, True or False values
+            """
+            return self.__isSolved
+
+    def ComputeScaleBetweenModels(cameraPoint1, cameraPoint2, cameraPoint3):
+        """
+        Compute scale between two models given the relative orientation
+
+        :param cameraPoints1: camera point in first camera space
+        :param cameraPoints2: camera point in second camera space
+        :param cameraPoints3:  camera point in third camera space
+
+        :type cameraPoints1: np.array 1x3
+        :type cameraPoints2: np.array 1x3
+        :type cameraPoints3: np.array 1x3
+
+
+        .. warning::
+
+            This function is empty, need implementation
+        """
+        pass
+
+    def RayIntersection(self, cameraPoints1, cameraPoints2, cameraPoints3):
+        """
+        Compute coordinates of the corresponding model point
+
+        :param cameraPoints1: points in camera1 coordinate system
+        :param cameraPoints2: points in camera2 coordinate system
+        :param cameraPoints3: points in camera3 coordinate system
+
+        :type cameraPoints1 np.array nx3
+        :type cameraPoints2: np.array nx3
+        :type cameraPoints3: np.array nx3
+
+        :return: point in model coordinate system
+        :rtype: np.array nx3
+
+        .. warning::
+
+            This function is empty' need implementation
+        """
+
+    def drawModles(self, imagePair1, imagePair2, modelPoints1, modelPoints2):
+        """
+        Draw two models in the same figure
+
+        :param imagePair1: first image pair
+        :param imagePair2:second image pair
+        :param modelPoints1: points in the firt model
+        :param modelPoints2:points in the second model
+
+        :type modelPoints1: np.array nx3
+        :type modelPoints2: np.array nx3
+
+        :return: None
+
+        .. warning::
+            This function is empty, need implementation
+        """
+
+
+if __name__ == '__main__':
+    camera = Camera(152, None, None, None, None)
+    image1 = SingleImage(camera)
+    image2 = SingleImage(camera)
+    image3 = SingleImage(camera)
+    imagePair1 = ImagePair(image1, image2)
+    imagePair2 = ImagePair(image2, image3)
+    imageTriple1 = ImageTriple(imagePair11, imagePair22)
--- Lab8/Photo-Lab8.py
+++ Lab8/Photo-Lab8.py
@@ -8,3 +8,16 @@ import MatrixMethods as mm
 from scipy import linalg as la
 
 if __name__ == "__main__" :
+    #  computing model link without scale
+    #  taking values from lab 7 --
+    R2 = np.array([[0.93736404, 0.17563438, -0.30083427], [-0.1699276, 0.984417, 0.04525236],
+                   [0.30409425, 0.00870211, 0.9526022]])
+    R23 = np.array([[0.95362705, 0.14610846, -0.26314977], [-0.14700069, 0.9890003, 0.01640695],
+                    [0.2626524, 0.02303708, 0.9646155]])
+    o2 = np.array([1, -0.14151914, 0.22903829])
+    b23 = np.array([1, -0.226124, 0.28123083])
+
+    R3 = np.dot(R2, R23)
+    o3 = o2 + np.dot(R2, b23)
+
+    print("hi")
