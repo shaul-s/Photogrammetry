3c031f8dfe85aa3374ee5645801be54a4514ae3d
oren lauterman <orenltr@gmail.com>
2021-04-02 13:22:22 +0300

    loading state and labels

--- Digital/Project/app.py
+++ Digital/Project/app.py
@@ -36,89 +36,109 @@ encoded_image_targets = base64.b64encode(open(image_filename, 'rb').read())
 
 app.layout = html.Div([
 
+    
+    html.H1("RAD Targets Detector", style={'text-align': 'center'}),
+    html.H2("Adjust parameters to get clear targets", style={'text-align': 'center'}),
+
+    html.Div(id='slider-d-output', style={'margin-top': 20}),
+    dcc.Slider(
+        id='d-value',
+        min=0,
+        max=25,
+        step=1,
+        marks={
+            1: '1',
+            5: '5',
+            10: '10',
+            15: '15',
+            20: '20',
+            25: '25'
+        },
+        value=10,
+    ),
+    
+    html.Div(id='slider-sigma-output', style={'margin-top': 20}),
+    dcc.Slider(
+        id='sigma',
+        min=0,
+        max=250,
+        step=10,
+        marks={
+            0: '0',
+            50: '50',
+            100: '100',
+            150: '150',
+            200: '200',
+            250: '250'
+        },
+        value=100,
+    ),
+    
+    html.Div(id='slider-b_size-output', style={'margin-top': 20}),
+    dcc.Slider(
+        id='b_size',
+        min=3,
+        max=31,
+        step=2,
+        marks={
+            0: '0',
+            5: '5',
+            10: '10',
+            15: '15',
+            20: '20',
+            25: '25',
+            30: '30'
+        },
+        value=15,
+    ),
+    
     # html.Div(
-        html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
-
-        dcc.Slider(
-            id='d-value',
-            min=0,
-            max=25,
-            step=1,
-            marks={
-                1: '1',
-                5: '5',
-                10: '10',
-                15: '15',
-                20: '20',
-                25: '25'
-            },
-            value=10,
-        ),
-        # html.Div(id='slider-output-container'),
-        
-        dcc.Slider(
-            id='sigma',
-            min=0,
-            max=250,
-            step=10,
-            marks={
-                0: '0',
-                50: '50',
-                100: '100',
-                150: '150',
-                200: '200',
-                250: '250'
-            },
-            value=100,
-        ),
-        dcc.Slider(
-            id='b_size',
-            min=3,
-            max=31,
-            step=2,
-            marks={
-                0: '0',
-                5: '5',
-                10: '10',
-                15: '15',
-                20: '20',
-                25: '25',
-                30: '30'
-            },
-            value=15,
-        ),
-        # html.Div(
-        # dcc.Graph(id = 'img', figure= fig)
-        # ),
-        html.Div([
-
-            html.Div([
-            html.Img(id= 'original_image',src='data:image/png;base64,{}'.format(encoded_image_original.decode()),style={'width': '100%', 'height': '100%'}),
-            ],style={'width': '49%', 'display': 'inline-block'}),
-
-            html.Div([
-            html.Img(id= 'binary_image',src='data:image/png;base64,{}'.format(encoded_image_binary.decode()),style={'width': '100%', 'height': '100%'})
-            ],style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
-        ]),
-
-        html.Div(html.Button('Find Rad Targets', id='submit-val', n_clicks=0,style={'float': 'center', 'display': 'inline-block'})),
-        html.Div(id='container-button-basic',
-             children='Enter a value and press submit', style={'float': 'center', 'display': 'inline-block'}),
+    # dcc.Graph(id = 'img', figure= fig)
+    # ),
+    html.Div([
 
         html.Div([
+        html.Img(id= 'original_image',src='data:image/png;base64,{}'.format(encoded_image_original.decode()),style={'width': '100%', 'height': '100%'}),
+        ],style={'width': '49%', 'display': 'inline-block'}),
 
-            # html.Div([
-            # html.Img(id= 'contours_image',src='data:image/png;base64,{}'.format(encoded_image_contours.decode()),style={'width': '100%', 'height': '100%'}),
-            # ],style={'width': '49%', 'display': 'inline-block'}),
+        html.Div([
+        html.Img(id= 'binary_image',src='data:image/png;base64,{}'.format(encoded_image_binary.decode()),style={'width': '100%', 'height': '100%'})
+        ],style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+    ]),
 
-            html.Div([
-            html.Img(id= 'targets_image',src='data:image/png;base64,{}'.format(encoded_image_targets.decode()),style={'width': '100%', 'height': '100%'})
-            ],style={ 'float': 'center', 'display': 'inline-block'})
-        ])
+    html.Div(html.Button('Find Rad Targets', id='submit-val', n_clicks=0,style={'horizontalAlign' : 'center', 'display': 'inline-block'})),
+    dcc.Loading(id = 'loading',
+    children = [html.Div(id='container-button-basic',
+                children='After adjusting binarization press to find targets', style={'horizontalAlign' : 'center', 'display': 'inline-block'})]),
+
+    html.Div([
+
+        # html.Div([
+        # html.Img(id= 'contours_image',src='data:image/png;base64,{}'.format(encoded_image_contours.decode()),style={'width': '100%', 'height': '100%'}),
+        # ],style={'width': '49%', 'display': 'inline-block'}),
+
+        html.Div([
+        html.Img(id= 'targets_image',src='data:image/png;base64,{}'.format(encoded_image_targets.decode()),style={'width': '100%', 'height': '100%'})
+        ],style={ 'float': 'center', 'display': 'inline-block'})
+    ])
         
 
 ])
 
+@app.callback(Output('slider-d-output', 'children'),
+              Input('d-value', 'drag_value'))
+def display_value(drag_value):
+    return f'd_value: {drag_value}'
+
+@app.callback(Output('slider-sigma-output', 'children'),
+              Input('sigma', 'drag_value'))
+def display_value(drag_value):
+    return f'sigma_value: {drag_value}'
+
+@app.callback(Output('slider-b_size-output', 'children'),
+            Input('b_size', 'drag_value'))
+def display_value(drag_value):
+    return f'b_size: {drag_value}'
 
 @app.callback(
     Output('binary_image', 'src'),
@@ -146,8 +166,21 @@ def update_image(d, sigma, b):
     encoded_image_binary = base64.b64encode(open(image_filename, 'rb').read())
     return 'data:image/png;base64,{}'.format(encoded_image_binary.decode())
 
+# @app.callback(
+#     Output('container-button-basic', 'children'),
+#     Input('submit-val', 'n_clicks'))
+# def running_indication(n_clicks):
+#     if n_clicks > 0:
+#         return 'Running...'
+#     return 'After adjusting binarization press to find targets (it might take some time...)'
+
+
+
 @app.callback(
+    [
     Output('targets_image', 'src'),
+    Output('container-button-basic', 'children')
+    ],
     [
     Input('submit-val', 'n_clicks')
     ]
@@ -169,8 +202,7 @@ def update_targets_image(n_clicks):
         c_img = rgb_img.copy()
         cv2.drawContours(c_img, contours, -1, (255, 0, 0), 2)
         plt.imsave('contours.png', c_img)
-        # image_filename = 'contours.png'
-        # encoded_image_contours = base64.b64encode(open(image_filename, 'rb').read())
+
 
         # targets image
         ellipses, hulls = rtd.find_ellipses(contours)
@@ -181,9 +213,12 @@ def update_targets_image(n_clicks):
         t_img = rgb_img.copy()
         rtd.draw_targets(t_img, targets_df)
         plt.imsave('targets.png', t_img)
+        image_filename = 'targets.png'
+        encoded_image_targets = base64.b64encode(open(image_filename, 'rb').read())
+        return 'data:image/png;base64,{}'.format(encoded_image_targets.decode()), f'{targets_df.shape[0]} targets found' 
     image_filename = 'targets.png'
     encoded_image_targets = base64.b64encode(open(image_filename, 'rb').read())
-    return 'data:image/png;base64,{}'.format(encoded_image_targets.decode())    
+    return 'data:image/png;base64,{}'.format(encoded_image_targets.decode()), 'After adjusting binarization press to find targets' 
 
 # app.run_server(mode="inline")
 if __name__ == '__main__':
